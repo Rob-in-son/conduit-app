73fc1695b6aa27c8f3bb12fdcca6ff361ad2c76a
Rob-in-son <estherchidinma202@gmail.com>
2024-09-11 14:58:44 +0100

    use default not value

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,25 +5,25 @@ variable "instance_name" {
 
 variable "ami_id" {
     description = "Ubuntu AMI"
-    value = "ami-0e86e20dae9224db8"
+    default = "ami-0e86e20dae9224db8"
 }
 
 variable "instance_type" {
     description = "Instance type"
-    value = "t2.micro"
+    default = "t2.micro"
 }
 
 variable "key_name" {
     description = "Name of Keypair"
-    value = "conduit-app-key" 
+    default = "conduit-app-key" 
 }
 
 variable "userdata_script" {
     description = "Script to be executed by userdata"
-    value = "./setup.sh"
+    default = "./setup.sh"
 }
 
 variable "region" {
     description = "Region"
-    value = "us-east-1"
+    default = "us-east-1"
 }
